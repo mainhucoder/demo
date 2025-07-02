6774c3b2ba1a45832b588747fe898fa215458e92
Uday gupta <uday4852.be@chitkara.edu.in>
2025-07-02 12:51:42 +0530

    commit

---
+++ Prac.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Prac
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter name");
+		String name = sc.nextLine();
+		System.out.println("Enter roll no.");
+		int a = sc.nextInt();
+	}
+}
